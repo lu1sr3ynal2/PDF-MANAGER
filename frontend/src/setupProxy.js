700a7e56317444ac5dbfaa29d372f33f4690fe28
lu1sr3ynal2 <itsluiss.rey@gmail.com>
2024-08-07 19:43:54 -0300

    Proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,11 +1,14 @@
+// setupProxy.js
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'http://localhost:8000',
-      changeOrigin: true,
-    })
-  );
+  if (process.env.NODE_ENV === 'development') {
+    app.use(
+      '/api',
+      createProxyMiddleware({
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      })
+    );
+  }
 };
