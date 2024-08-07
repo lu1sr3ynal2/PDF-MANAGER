9c2cde6410936ee995e080b56a7c44d81ce47edf
lu1sr3ynal2 <itsluiss.rey@gmail.com>
2024-08-07 17:34:07 -0300

    Update API base URL to use environment variable

--- run.py
+++ run.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     sys.path.append("backend")
 
     # Ejecuta el servidor Uvicorn con la aplicación de FastAPI
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
